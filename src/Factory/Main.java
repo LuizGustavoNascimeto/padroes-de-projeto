3dd05d5d92e0355f76eeeffe07b4210ffccecccb
Jordan <jordanduzabonfim@gmail.com>
2024-12-04 17:08:17 -0300

    Implementa produtos concretos circulo e retangulo

--- src/Factory/Main.java
+++ src/Factory/Main.java
@@ -1,14 +1,25 @@
 public class Main {
-
-    public class FactoryMethodShapes {
-
-        // Produto: Interface comum para todas as formas
         interface Forma {
             void desenhar();
         }
 
+        // Produto Concreto 1: Círculo
+        static class Circulo implements Forma {
+            @Override
+            public void desenhar() {
+                System.out.println("Desenhando um círculo.");
+            }
+        }
+
+        // Produto Concreto 2: Retângulo
+        static class Retangulo implements Forma {
+            @Override
+            public void desenhar() {
+                System.out.println("Desenhando um retângulo.");
+            }
+        }
+
         public static void main(String[] args) {
-            // Código principal será adicionado no futuro.
+            // Código principal será adicionado nos próximos commits.
         }
     }
-}
