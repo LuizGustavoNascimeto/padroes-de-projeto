fd0a99aa58e662ba1fa0e43eb87099a585622999
Jordan <jordanduzabonfim@gmail.com>
2024-12-04 17:10:41 -0300

    Define classe abstrata FormaFactory

--- src/Factory/Main.java
+++ src/Factory/Main.java
@@ -1,25 +1,34 @@
 public class Main {
-        interface Forma {
-            void desenhar();
-        }
 
-        // Produto Concreto 1: Círculo
-        static class Circulo implements Forma {
-            @Override
-            public void desenhar() {
-                System.out.println("Desenhando um círculo.");
-            }
-        }
+    interface Forma {
+        void desenhar();
+    }
 
-        // Produto Concreto 2: Retângulo
-        static class Retangulo implements Forma {
-            @Override
-            public void desenhar() {
-                System.out.println("Desenhando um retângulo.");
-            }
-        }
-
-        public static void main(String[] args) {
-            // Código principal será adicionado nos próximos commits.
+    static class Circulo implements Forma {
+        @Override
+        public void desenhar() {
+            System.out.println("Desenhando um círculo.");
         }
     }
+
+    static class Retangulo implements Forma {
+        @Override
+        public void desenhar() {
+            System.out.println("Desenhando um retângulo.");
+        }
+    }
+
+    // Criador: Classe abstrata com o método de fábrica
+    static abstract class FormaFactory {
+        public void criarEDesenharForma() {
+            Forma forma = criarForma();
+            forma.desenhar();
+        }
+
+        protected abstract Forma criarForma();
+    }
+
+    public static void main(String[] args) {
+        // Código principal será adicionado nos próximos commits.
+    }
+}
