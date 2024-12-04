b60a827b50d8e0d9a60488ec21ba7695a3c55a43
Jordan <jordanduzabonfim@gmail.com>
2024-12-04 17:02:17 -0300

    Define interface Forma

---
+++ src/Factory/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public class FactoryMethodShapes {
+
+        // Produto: Interface comum para todas as formas
+        interface Forma {
+            void desenhar();
+        }
+
+        public static void main(String[] args) {
+            // Código principal será adicionado no futuro.
+        }
+    }
+}
